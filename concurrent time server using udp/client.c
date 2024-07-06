64c11c236c3277a2be1d6ecfbab8a4144090571c
Eeva Eldhose <97470217+Eeva3003@users.noreply.github.com>
2024-07-06 12:04:45 +0530

    Update client.c

--- concurrent time server using udp/client.c
+++ concurrent time server using udp/client.c
@@ -12,18 +12,17 @@
 // Driver 
  int main() {
 int sockfd;
-char 
-buffer[MAXLINE]; time_t t;
+char buffer[MAXLINE]; 
+time_t t;
 time(&t);
 char *hello = ctime(&t); //"Hello from client : Current Time is ";
 // char *currtime = 
-ctime(&t); struct 
-sockaddr_in
-servaddr;
+ctime(&t);
+struct sockaddr_in servaddr;
 // printf("\n current time is : %s",ctime(&t));
 // Creating socket file descriptor
-if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) 
-< 0 ) { perror("socket creation failed"); 
+if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) 
+{ perror("socket creation failed"); 
 exit(EXIT_FAILURE);
 }
 memset(&servaddr, 0, sizeof(servaddr));
@@ -33,14 +32,9 @@ servaddr.sin_port = htons(PORT);
 servaddr.sin_addr.s_addr = 
 INADDR_ANY;
 int n, len;
-sendto(sockfd, (const char *)hello, strlen(hello), 
-MSG_CONFIRM, (const struct sockaddr *) 
-&servaddr,
-sizeof(servaddr)); 
+sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &servaddr,sizeof(servaddr)); 
 printf("Hello TIME  sent to the Server.\n");
-n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-MSG_WAITALL, (struct sockaddr *) 
-&servaddr, &len);
+n = recvfrom(sockfd, (char *)buffer, MAXLINE,MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
 buffer[n] = '\0';
 printf("Server TIME : %s\n", buffer);
 close(sockfd); 
